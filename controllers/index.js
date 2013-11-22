7cbbbf679c449e39815a1168726fecf98399383e
lmarkus <lmarkus@paypal.com>
2013-11-22 12:16:05 -0800

    Added random language picker

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,14 @@ module.exports = function (server) {
 
     server.get('/', function (req, res) {
         var model = { name: 'kraken-localization' };
-        
+
+        var languages = ["en-US", "fr-CA", "es-ES", "tlh-US"];
+        res.locals.context = {
+            locality: languages[ parseInt(Math.random() * 4) ]
+        };
+
         res.render('index', model);
-        
+
     });
 
 };
